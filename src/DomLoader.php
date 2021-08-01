6d5adb93679dc2c78254aa6ab5c144475ccb6a3b
radiocat <radiocat.@radiocatmbp.lan>
2021-08-01 15:24:31 +0900

    PHPHTMLParserを使ってDomをロードするクラスを作成

---
+++ src/DomLoader.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Gomicale;
+
+use PHPHtmlParser\Dom;
+use PHPHtmlParser\Options;
+
+class DomLoader 
+{
+    private $dom;
+
+    /**
+     * @param $url Domのロード先URL
+     */
+    public function __construct(string $url)
+    {
+        $options = new Options();
+        $options->setEnforceEncoding('utf8');
+        $this->dom = new Dom();
+        $this->dom->loadFromUrl($url, $options);
+    }
+
+    public function getDom(): Dom
+    {
+        return $this->dom;
+    }
+    
+}
